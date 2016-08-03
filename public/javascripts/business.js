95efec78ec5bffe2c332c5292c36ebfe974db94f
Stephen Fox <stephenfox995@hotmail.com>
2016-08-04 00:07:56 +0100

    Products for a business can now be shown.

---
+++ public/javascripts/business.js
@@ -0,0 +1,13 @@
+var app = angular.module('business', []);
+
+app.controller('productsController', function ($scope, $http) {
+  $scope.products = [];
+  var businessID = 'rkIeje2r';
+  $http.get('/business/' + businessID + '/products')
+    .success(function (data) {
+      $scope.products = data.products;
+    })
+    .error(function (data) {
+
+  })
+});
