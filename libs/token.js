257566f2ae7ab1927e8074dda207a2c361fef0d5
Stephen Fox <stephenfox995@hotmail.com>
2016-08-09 20:25:07 +0100

    Updated parsin of token to be asynchronous

--- libs/token.js
+++ libs/token.js
@@ -2,22 +2,31 @@
 
 var httpCodes = require('./httpCodes'),
     jwt       = require('jsonwebtoken'),
-    ms    = require('ms');
+    ms        = require('ms');
 
 var token = exports;
 
 const secret = 'SOME_SECRET';
 
-token.parseAuthHeader = function (req) {
+
+
+/***
+ * Parses HTTP headers for the authorization header value.
+ *
+ * @param req
+ *
+ * @param {function(string|null)} cb - If successfully parsed the token will be passed
+ *                                     via the callback, otherwise null.
+ */
+token.parseAuthHeaderToken = function (req, cb) {
   var bearerToken = null;
   var bearerHeader = req.headers['authorization'];
   if (typeof bearerHeader !== 'undefined') {
     var bearer = bearerHeader.split(' ');
     bearerToken = bearer[1];
-    req.token = bearerToken;
-    return true;
+    cb(bearerToken);
   } else {
-    return false;
+    cb(null);
   }
 };
 
@@ -28,19 +37,20 @@ token.parseAuthHeader = function (req) {
  * If the token is not valid a response will be sent.
  * */
 token.validToken = function (req, res, next) {
-  if (token.parseAuthHeader(req)) {
-    jwt.verify(req.token, secret, function (err, decoded) {
-      console.log(Math.floor(Date.now() / 1000));
-      if (err) {
-        return res.json({ success:false, responseMessage: "Failed to authenticate token. Reason: " + err.message });
-      }
-      req.decoded = decoded;
-      next();
-    });
-  }
-  else {
-    return res.json({ success:false, responseMessage: "No token provided." });
-  }
+  token.parseAuthHeaderToken(req, function (token) {
+    if (token) {
+      jwt.verify(token, secret, function (err, decoded) {
+        if (err) {
+          return res.json({ success:false, responseMessage: "Failed to authenticate token. Reason: " + err.message });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    }
+    else {
+      return res.json({ success:false, responseMessage: "No token provided." });
+    }
+  });
 };
 
 
