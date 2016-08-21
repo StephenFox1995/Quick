fa59c8d1018a2816a41f7c12b17d2d1eec764ea3
Stephen Fox <stephenfox995@hotmail.com>
2016-08-21 17:40:55 +0100

    Updated angular for register bbusiness and users.

--- public/javascripts/authenticate.js
+++ public/javascripts/authenticate.js
@@ -1,58 +1,63 @@
-'use strict';
+(function () {
+  'use strict';
 
-var app =
-  angular
-    .module('authenticate', [
-      'routeController',
-      'session'
-    ])
-    .controller('AuthenticateController', AuthenticateController)
-    .factory('authService', authService);
+  var app =
+    angular
+      .module('authenticate', [
+        'routeController',
+        'session'
+      ])
+      .controller('AuthenticateController', AuthenticateController)
+      .factory('authService', authService);
 
-AuthenticateController.inject = ['$scope', 'whereTo', 'authService'];
-
-function AuthenticateController($scope, whereTo, authService) {
-  $scope.httpBody = {
-    authType: 'user'
-  };
-  $scope.authenticate = function () {
-    // Attempt to authenticate the user.
-    authService.authenticate($scope.httpBody, function (err, data) {
-      if (err) {
-        return $scope.message = data.responseMessage;
-      }
-      whereTo.nextRoute(app.LOGIN);
-    });
-  }
-}
+  AuthenticateController.inject = ['$scope', 'whereTo', 'authService'];
 
 
-authService.inject = ['$http'];
-function authService($http) {
-  return {
-    authenticate: authenticate
-  };
-
-  function authenticate(httpBody, callback) {
-    $http.post('/authenticate', httpBody)
-      .success(function (data) {
-        verify(data, function (verified) {
-          return verified ? callback(null, data) : callback(new Error(), data);
-        });
-      })
-      .error(function (data) {
-        return callback(new Error(), data);
+  function AuthenticateController($scope, whereTo, authService) {
+    $scope.httpBody = {
+      authType: 'user'
+    };
+    $scope.authenticate = function () {
+      // Attempt to authenticate the user.
+      authService.authenticate($scope.httpBody, function (err, data) {
+        if (err) {
+          return $scope.message = data.responseMessage;
+        }
+        whereTo.nextRoute(app.LOGIN);
       });
-  }
-
-  function verify(data, callback) {
-    if (data.success) {
-      return callback(true);
-    } else {
-      return callback(false);
     }
   }
-}
+
+
+  authService.inject = ['$http'];
+  function authService($http) {
+    return {
+      authenticate: authenticate
+    };
+
+    function authenticate(httpBody, callback) {
+      $http.post('/authenticate', httpBody)
+        .success(function (data) {
+          verify(data, function (verified) {
+            return verified ? callback(null, data) : callback(new Error(), data);
+          });
+        })
+        .error(function (data) {
+          return callback(new Error(), data);
+        });
+    }
+
+    function verify(data, callback) {
+      if (data.success) {
+        return callback(true);
+      } else {
+        return callback(false);
+      }
+    }
+  }
+}());
+
+
 
 
 
--- public/javascripts/signUp.js
+++ public/javascripts/signUp.js
@@ -1,57 +1,54 @@
-var app = angular.module('signUp', []);
-
-app.controller('signUpController',
-  ['$scope',
-    'signUpServices',
-    function ($scope, signUpServices) {
-      $scope.httpBody = {};
-      $scope.message = '';
-      $scope.signUpModel = {};
-
-      $scope.signUp = function () {
-        if (!$scope.signUpModel.endPoint) { return; }
-        var endPoint = $scope.signUpModel.endPoint;
-
-        // Begin sign up process.
-        signUpServices.signUp(endPoint, $scope.httpBody, function (reponse) {
-          signUpServices.verifyResponse(reponse, function (success, message) {
-            if (success) {
-              // Log user in etc.
-              $scope.message = message;
-            } else {
-              // Display message.
-              $scope.message = message;
-            }
-          });
-        });
-      };
-    }
-  ]);
+(function () {
+  'use strict';
+  var app =
+    angular
+      .module('signUp', [])
+      .controller('SignUpController', SignUpController)
+      .factory('signUpService', signUpService);
+  
+  SignUpController.inject = ['$scope', 'signUpService'];
+  function SignUpController($scope, signUpService) {
+    $scope.httpBody = {};
+    $scope.message = '';
+    $scope.signUpModel = {};
 
 
-/**
- * Use this service for sign up procedures for both users and businesses.
- * */
-app.factory('signUpServices', ['$http', function ($http) {
-  return {
-    signUp: function (endPoint, httpBody, callback) {
-      $http.post(endPoint, httpBody)
-        .success(function (response) {
-          callback(response);
-        })
-        .error(function (response) {
-          callback(response);
-        });
-    },
+    $scope.signUp = function () {
+      if (!$scope.signUpModel.endPoint) { return; }
+      var endPoint = $scope.signUpModel.endPoint;
 
-    verifyResponse: function (response, callback) {
-      if (response.success) {
-        // Successful sign up.
-        return callback(true, response.responseMessage);
-      } else {
-        // Failed to sign up.
-        return callback(false, response.responseMessage);
-      }
+      signUpService.signUp(endPoint, $scope.httpBody, function (err, response) {
+        if (err) {
+          return $scope.message = 'Failed to sign up.';
+        }
+        $scope.message = 'Sign up successful';
+      })
     }
   }
-}]);
+
+  signUpService.inject = ['$http'];
+  function signUpService($http) {
+    return {
+      signUp: signUp
+    };
+
+    function signUp(endPoint, httpBody, callback) {
+      $http.post(endPoint, httpBody)
+        .success(function (data) {
+          verify(data) ? callback(null, data): callback(new Error(), data);
+        })
+        .error(function (data) {
+          callback(new Error(), data);
+        });
+    }
+    
+    function verify(data) {
+      if (data.success) {
+        return true;
+      } else {}
+      return false;
+    }
+  }
+}());
+
+
