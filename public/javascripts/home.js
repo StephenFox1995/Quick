4f251a99461815fc7409f4552f45c888e9bb2130
Stephen Fox <stephenfox995@hotmail.com>
2016-08-22 21:32:39 +0100

    Home now shows recent purchases

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -6,14 +6,39 @@
       'session',
       'purchase'
     ])
-    .controller('HomeController', HomeController);
+    .controller('HomeController', HomeController)
+    .factory('setupHomeViews', setupHomeViews);
 
 
-  HomeController.inject = ['$scope', 'purchaseService'];
-  function HomeController($scope, purchaseService) {
-    purchaseService.getUserPurchases(function (data) {
-      console.log(data);
+  HomeController.inject = ['$scope', 'setupHomeViews'];
+  function HomeController($scope, setupHomeViews) {
+    $scope.purchases = null;
+
+    setupHomeViews.purchasesView(function (err, data) {
+      if (err) {
+        // TODO: Display error.
+      }
+      $scope.purchases = data.purchases;
     });
+  }
 
+
+
+
+  setupHomeViews.inject = ['purchaseService'];
+  function setupHomeViews(purchaseService) {
+    return {
+      purchasesView: setupRecentPurchases
+    };
+    
+    function setupRecentPurchases(callback) {
+      purchaseService.getUserPurchases(function (data) {
+        validateResponse(data) ? callback(null, data): callback(new Error, data);
+      });
+    }
+    
+    function validateResponse(data) {
+      return data.success;
+    }
   }
 }());
