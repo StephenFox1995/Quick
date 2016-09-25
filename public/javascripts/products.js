2048009c6bd2082fe94a33a32295ec8d16e2e6c7
Stephen Fox <stephenfox995@hotmail.com>
2016-09-26 00:51:54 +0100

    Added function to check for differences between products that have been edited before and after

--- public/javascripts/products.js
+++ public/javascripts/products.js
@@ -8,7 +8,9 @@
   function productsService($http) {
     return {
       getProducts: getProducts,
-      addProduct: addProduct
+      addProduct: addProduct,
+      updateProduct: updateProduct,
+      generateHTTPBodyForProductUpdate: generateHTTPBodyForProductUpdate
     };
 
     /**
@@ -39,6 +41,73 @@
       .error(function (data) {
         callback(new Error('There was an error posting the product'));
       });
+    }
+
+    /**
+     * Attempts to update a product in the database.
+     * @param {Object} data - The product data.
+     * @param {function(err, data)} callback - Callback function.
+     */
+    function updateProduct(data, callback) {
+      $http.patch('/product', product)
+      .success(function (data) {
+        callback(null, data);
+      }).error(function (data) {
+        callback(new Error('There was an error updating the product.'));
+      });
+    }
+
+    /**
+     * Generates the HTTP body needed for updating
+     * a product in the database.
+     * 
+     * @param {object} productBeforeEdit - The product before edit.
+     * @param {object} productAfterEdit - The product after edit.
+     * @return {object} The HTTP body containing the changed fields.
+     */
+    function generateHTTPBodyForProductUpdate(productBeforeEdit, productAfterEdit) {
+      if (productBeforeEdit == null || productAfterEdit == null) {
+        return {};
+      }
+
+      var editedProduct = {
+        id: productBeforeEdit.id,
+        updateFields: []
+      };
+      var EditedField = {
+        column: "",
+        newValue: ""
+      };
+      
+      // Check the following fields for change
+      // - name
+      // - specifiedID
+      // - description
+      // - price
+      if (productAfterEdit.name !== productBeforeEdit.name) {
+        var editedName = new EditedField();
+        editedName.column = "name";
+        editedName.newValue = productAfterEdit.name;
+        editedProduct.updateFields.push(editedName);
+      }
+      if (productAfterEdit.specifiedID !== productBeforeEdit.specifiedID) {
+        var editedSpecifiedID = new EditedField();
+        editedSpecifiedID.column = 'specifiedID';
+        editedSpecifiedID.newValue = productAfterEdit.specifiedID;
+        editedProduct.updateFields.push(editedSpecifiedID);
+      }
+      if (productAfterEdit.description !== productBeforeEdit.description) {
+        var editedDescription = new EditedField();
+        editedDescription.column = 'description';
+        editedDescription.newValue = productAfterEdit.description;
+        editedProduct.updateFields.push(editedDescription);
+      }
+      if (productAfterEdit.price !== productBeforeEdit.price) {
+        var editedPrice = new EditedField();
+        editedPrice.column = 'price';
+        editedPrice.newValue = productAfterEdit.price;
+        editedProduct.updateFields.push(editedPrice);
+      }
     }
   }
 })();
