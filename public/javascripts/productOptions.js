981f6c23a04f065392c6b19fa6985cb7ce8168de
Stephen Fox <stephenfox995@hotmail.com>
2016-10-01 00:55:12 +0100

    Updated javascript styling in productOptions directive file

--- public/javascripts/productOptions.js
+++ public/javascripts/productOptions.js
@@ -12,52 +12,54 @@
       restrict: 'E',
       require: '^',
       templateUrl: '/templates/directives/productOptions.html',
-      controller: function ($scope) {
-        // All Product Options.
-        $scope.productOptions = [];
-
-        $scope.newProductOption = function (name) {
-          if (name === undefined || name === null || name === "") {
-            return;
-          }
-          // Check that product option doesnt already exsist.
-          var optionAlreadyExists = false;
-          angular.forEach($scope.productOptions, function (productOption, index) {
-            if (productOption.name === name) {
-              optionAlreadyExists = true;
-            }
-          });
-
-          if (!optionAlreadyExists) {
-            var productOption = new ProductOption(name);
-            $scope.productOptions.push(productOption);
-            $scope.newProductOptionName = null;
-          }
-        };
-
-        $scope.removeProductOption = function (productOption) {
-          if (!productOption) { return; }
-
-          angular.forEach($scope.productOptions, function (option, index) {
-            if (option === productOption) {
-              $scope.productOptions.splice(index, 1);
-            }
-          });
-        };
-
-        /** Adds a new ProductOption value.
-         * @param {ProductOption} productOption - The ProductOption to add the value to.
-         * @param {string} valueName - The name of the value.
-         * @param {float} priceDelta - The price delta.
-         */
-        $scope.addProductOptionValue = function (productOption, valueName, priceDelta) {
-          productOption.addValue(valueName, priceDelta);
-        };
-
-        $scope.removeProductOptionValue = function (productOption, valueName) {
-          productOption.removeValue(valueName);
-        };
-      }
+      controller: Controller
     };
-  }
+
+    function Controller($scope) {
+      // All Product Options.
+      $scope.productOptions = [];
+
+      $scope.newProductOption = function (name) {
+        if (name === undefined || name === null || name === "") {
+          return;
+        }
+        // Check that product option doesnt already exsist.
+        var optionAlreadyExists = false;
+        angular.forEach($scope.productOptions, function (productOption, index) {
+          if (productOption.name === name) {
+            optionAlreadyExists = true;
+          }
+        });
+
+        if (!optionAlreadyExists) {
+          var productOption = new ProductOption(name);
+          $scope.productOptions.push(productOption);
+          $scope.newProductOptionName = null;
+        }
+      };
+
+      $scope.removeProductOption = function (productOption) {
+        if (!productOption) { return; }
+
+        angular.forEach($scope.productOptions, function (option, index) {
+          if (option === productOption) {
+            $scope.productOptions.splice(index, 1);
+          }
+        });
+      };
+
+      /** Adds a new ProductOption value.
+       * @param {ProductOption} productOption - The ProductOption to add the value to.
+       * @param {string} valueName - The name of the value.
+       * @param {float} priceDelta - The price delta.
+       */
+      $scope.addProductOptionValue = function (productOption, valueName, priceDelta) {
+        productOption.addValue(valueName, priceDelta);
+      };
+
+      $scope.removeProductOptionValue = function (productOption, valueName) {
+        productOption.removeValue(valueName);
+      };
+    }
+  };
 })();
