1be88588eae4d752e27fe76f3ce9955693a40992
Stephen Fox <stephenfox995@hotmail.com>
2016-10-01 00:53:18 +0100

    Made ProductOptions as its own directive

--- public/javascripts/businessProducts.js
+++ public/javascripts/businessProducts.js
@@ -3,11 +3,12 @@
 
   angular
     .module('businessProducts', [
-      'products', 
+      'products',
       'session', 
       'dropper',
       'alert',
-      'validNumber'
+      'validNumber',
+      'productOptions'
     ])
     .controller('BusinessProductsController', BusinessProductsController)
     .controller('BusinessProductsCreationController', BusinessProductsCreationController);
@@ -133,15 +134,12 @@
   }
 
 
-  BusinessProductsCreationController.inject = ['$scope', 'productsService', 'sessionService', '$location', '$anchorScroll', 'ProductOption'];
-  function BusinessProductsCreationController($scope, productsService, sessionService, $location, $anchorScroll, ProductOption) {
+  BusinessProductsCreationController.inject = ['$scope', 'productsService', 'sessionService', '$location', '$anchorScroll'];
+  function BusinessProductsCreationController($scope, productsService, sessionService, $location, $anchorScroll) {
     $scope.product;
     $scope.httpBody = {};
     $scope.showAlert = false;
-    $scope.productOptions = [];
-    $scope.showConfigureOptions = false;
-    $scope.showButtonsDefault = true;
-    $scope.optionName = "";
+    
     
     
     $scope.addProduct = function() {
@@ -164,53 +162,6 @@
         $scope.alertTitle = 'Success';
         $scope.alertMessage = 'Product was added!';
       });
-    };
-    
-    
-    /**
-     * @param {string} name - The name of the ProductOption
-     * @param {int} index - The index of the product option in the list.
-     */
-    $scope.newProductOption = function(name) {
-      if (name === undefined || name === null || name === "") {
-        return;
-      }
-      // Check that product option doesnt already exsist.
-      var optionAlreadyExists = false;
-      angular.forEach($scope.productOptions, function(productOption, index) {
-        if (productOption.name === name) {
-          optionAlreadyExists = true;
-        }
-      });
-
-      if (!optionAlreadyExists) {
-        var productOption = new ProductOption(name);
-        $scope.productOptions.push(productOption);
-        $scope.newProductOptionName = null;
-      }
-    };
-
-    $scope.removeProductOption = function(productOption) {
-      if (!productOption) { return; }
-
-      angular.forEach($scope.productOptions, function(option, index) {
-        if (option === productOption) {
-          $scope.productOptions.splice(index, 1);
-        }
-      });
-    };
- 
-    /** Adds a new ProductOption value.
-     * @param {ProductOption} productOption - The ProductOption to add the value to.
-     * @param {string} valueName - The name of the value.
-     * @param {float} priceDelta - The price delta.
-     */
-    $scope.addProductOptionValue = function(productOption, valueName, priceDelta) {
-      productOption.addValue(valueName, priceDelta);
-    };
-    
-    $scope.removeProductOptionValue = function(productOption, valueName) {
-      productOption.removeValue(valueName);
     };
     
     /** Scrolls to the bottom of the page.*/
