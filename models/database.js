189ef352a661af273a432db8be03602d597eacbe
Stephen Fox <stephenfox995@hotmail.com>
2016-10-06 17:09:42 +0100

    Removed all legacy SQLite code.

--- models/database.js
+++ models/database.js
@@ -1,93 +1,11 @@
 'use strict';
 
 var 
-  dbManager = require('./dbManager'),
   fs        = require('fs'),
   globals   = require('../libs/globals'),
   mongoose  = require('mongoose');
 
 var database = exports;
-
-/**
- * Inserts a User into the database.
- * @param   {User} user - The user to add to the database.
- * @param   {function(err)} cb - callback Callback function.
- **/
-database.insertUser = function (user, cb) {
-  dbManager.insertUser(user, cb);
-};
-
-database.getUser = function (email, callback) {
-  dbManager.getUser(email, callback);
-};
-
-database.getUserInfo = function (id, callback) {
-  dbManager.getUserInfo(id, callback);
-};
-
-database.getAllUsers = function (callback) {
-  dbManager.getAllUsers(callback);
-};
-database.getUserPurchases = function (userID, callback) {
-  dbManager.getUserPurchases(userID, callback);
-};
-
-database.getClient = function (clientID, clientSecret, callback) {
-  dbManager.getClient(clientID, clientSecret, callback);
-};
-
-/**
- * Insert an Product into the datase.
- * @param   {Product} product - product The order to add to the database..
- * @param   {function} cb - Callback function.
- * */
-database.insertProduct = function (product, cb) {
-  dbManager.insertProduct(product, cb);
-};
-database.getProduct = function(productID, callback) {
-  dbManager.getProduct(productID, callback);
-};
-database.getAllBusinessProducts = function (businessID, callback) {
-  dbManager.getAllBusinessProducts(businessID, callback);
-};
-database.updateProduct = function(productID, updateFields, callback) {
-  dbManager.updateProduct(productID, updateFields, callback);
-};
-
-
-/**
- * Inserts a Business into the database.
- * @param {Business} business - The business to add to the database.
- * @param {function} callback - Callback function.
- **/
-database.insertBusiness = function (business, callback) {
-  dbManager.insertBusiness(business, callback);
-};
-database.getAllBusiness = function (callback) {
-  dbManager.getAllBusiness(callback);
-};
-database.getBusiness = function (email, callback) {
-  dbManager.getBusiness(email, callback);
-};
-database.getBusinessInfo = function (id, callback) {
-  dbManager.getBusinessInfo(id, callback);
-};
-database.getBusinessPurchases = function(id, callback) {
-  dbManager.getBusinessPurchases(id, callback);
-};
-
-
-
-/**
- * Inserts a Purchase into the database.
- * @param   {Purchase} purchase - The purchase details to add to the database.
- * @param   {function} callback - The callback function.
- * */
-database.insertPurchase = function (purchase, callback) {
-  dbManager.insertPurchase(purchase, callback);
-};
-
-
 
 database.setupMongo = function(callback) {
   var configFileContents;
