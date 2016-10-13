e97ca84c964aff17b1e6e2fa5436b3ca9b97fec1
Stephen Fox <stephenfox995@hotmail.com>
2016-10-14 00:28:04 +0100

    Removed unused methods from User

--- libs/User.js
+++ libs/User.js
@@ -24,21 +24,6 @@ function User() { }
 User.prototype.schema = models.User;
 
 
-/**
- * Parses a JSON object from a POST request.
- * @param   {object} req - The request.
- * @param   {function(err)} cb - Callback function.
- * @return  {undefined}
- * */
-User.prototype.parsePOST = function(req, cb) {
-  if (validRequest(req)) {
-    this.setAttributesFromRequest(req);
-    return cb(null);
-  } else {
-    return cb(new Error('Could not parse user.'));
-  }
-};
-
 
 /**
  * A helper function to insert the current user object into the database.
@@ -106,42 +91,6 @@ User.prototype.verify = function (cb) {
     });
   });
 };
-  
-
-/**
- * Sets the attributes of the user object based on
- * the requests properties.
- * @param   {Object} req - The request.
- */
-User.prototype.setAttributesFromRequest = function(req) {
-  var user = req.body.user;
-  this.firstname = user.firstname;
-  this.lastname = user.lastname;
-  this.password = user.password;
-  this.email = user.email;
-};
-
-/**
- * Checks if a req is valid.
- * The request is deemed valid if the 
- * body contains a user object with the correct fields.
- * @param {Object} req The request.
- * @return {boolean} True if the req is valid.
- */
-function validRequest(req) {
-  if (!('user' in req.body)) {
-    return false;    
-  }
-  var user = req.body.user;
-  if (util.isValidString(user.firstname) &&
-      util.isValidString(user.lastname)  &&
-      util.isValidString(user.email)     &&
-      util.isValidString(user.password)) {
-    return true;
-  } else {
-    return false;
-  }
-}
 
 
 module.exports = User;
