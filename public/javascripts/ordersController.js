961342209af086523c60716d0ffa8bb613ef4d23
Stephen Fox <stephenfox995@hotmail.com>
2016-10-14 19:30:22 +0100

    Orders for a business can now be displayed. Updated orders.ejs interface.

--- public/javascripts/ordersController.js
+++ public/javascripts/ordersController.js
@@ -4,14 +4,15 @@
   .module('orders')
   .controller('OrdersController', OrdersController);
 
-  OrdersController.inject = ['$scope', 'ordersService'];
-  function OrdersController($scope, ordersService) {
+  OrdersController.inject = ['$scope', 'ordersService', 'sessionService'];
+  function OrdersController($scope, ordersService, sessionService) {
+    $scope.businessName = sessionService.getClientName();
     $scope.orders = [];
+
     (function getOrders() {
       ordersService.getOrders(function(err, data) {
         if (err) { /**Display to business that orders could not be displayed. */}
         $scope.orders = data.orders;
-        console.log($scope.orders);
       });
     })();    
   };
